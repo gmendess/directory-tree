ccad221e00c7f2cca05d4f173a894613a4d403be
Gabriel Mendes <gabriel.mendespng@gmail.com>
2020-02-25 23:29:56 -0300

    Criando structs que representam diretórios e arquivos

--- directory/directory.h
+++ directory/directory.h
@@ -1,4 +1,16 @@
 #ifndef _DIRECTORY_H_
 #define _DIRECTORY_H_
 
+#include <time.h>
+#include "../file/file.h"
+
+typedef struct directory {
+  char name;
+  struct directory* next;     // diretórios irmãos
+  struct directory* sub_dirs; // diretórios filhos
+  struct file* files;         // arquivos presentes no diretório
+  struct tm creation_time;    // data de criação do diretório
+} Directory;
+
+
 #endif // _DIRECTORY_H_
--- file/file.h
+++ file/file.h
@@ -1,4 +1,14 @@
 #ifndef _FILE_H_
 #define _FILE_H_
 
+#include <time.h>
+#include "../directory/directory.h"
+
+typedef struct file {
+  char* name;
+  struct file* next; // arquivos irmãos, presentes no mesmo diretório
+  struct tm creation_time; // data de criação do arquivo
+} File;
+
+
 #endif // _FILE_H_
