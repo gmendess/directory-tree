b505a8b3611006429e6da22d6f513c29c08ce7a9
Gabriel Mendes <gabriel.mendespng@gmail.com>
2020-02-26 20:30:21 -0300

    Criando variáveis globais static 'root' e 'wd' (working directory), e implementando função que inicializa diretório root e funções get para retornar root e wd

--- directory/directory.c
+++ directory/directory.c
@@ -3,3 +3,30 @@
 #include <time.h>
 #include "directory.h"
 #include "../file/file.h"
+
+// Variável que representa o diretório raiz da árvore de diretórios
+static Directory root;
+
+// Variável que representa o diretório atual (working directory)
+static Directory* wd;
+
+void init(void) {
+  time_t curr_time = time(NULL);
+
+  root.name = "root";
+  root.next = NULL;
+  root.sub_dirs = NULL;
+  root.files = NULL;
+  root.creation_time = localtime(&curr_time);
+
+  // inicialmente pwd aponta para o diretório raiz
+  wd = &root;
+}
+
+Directory get_root_dir(void) {
+  return root;
+}
+
+Directory pwd(void) {
+  return *wd;
+}
