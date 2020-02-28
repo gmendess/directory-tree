2b74ac01801b189f3b1bb6f193f2dbbf30543c92
Gabriel Mendes <gabriel.mendespng@gmail.com>
2020-02-27 22:01:04 -0300

    Adicionando diretório 'utils' ao projeto, que conterá algumas funções e macros utilitárias

---
+++ utils/utils.c
@@ -0,0 +1,11 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+#include "utils.h"
+
+struct tm* time_now(void) {
+  time_t t = time(NULL);
+  struct tm* _time_now = localtime(&t);
+
+  return _time_now;
+}
---
+++ utils/utils.h
@@ -0,0 +1,10 @@
+#ifndef _UTILS_H_
+#define _UTILS_H_
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+struct tm* time_now(void);
+
+#endif // _UTILS_H_
