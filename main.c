024ddaf95664699b5ec7855939f04ffbe258e79b
Gabriel Mendes <gabriel.mendespng@gmail.com>
2020-02-27 21:59:57 -0300

    Alguns testes

--- main.c
+++ main.c
@@ -2,8 +2,19 @@
 #include <stdio.h>
 #include "file/file.h"
 #include "directory/directory.h"
+#include "utils/utils.h"
 
 int main() {
+
+  init();
+  Directory r = get_root_dir();
+  printf("%s\n", r.name);
+
+  Directory wd = pwd();
+  printf("%s\n", wd.name);
+
+  Directory* new_dir = alloc_directory("jogos");
+  printf("%s\n", new_dir->name);
 
   return EXIT_SUCCESS;
 }
