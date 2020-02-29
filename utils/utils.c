1e9d1ce01c2547d597d1b20c92d618d2614c522d
Gabriel Mendes <gabriel.mendespng@gmail.com>
2020-02-29 17:17:04 -0300

    Criando função para copiar o conteúdo apontado por um ponteiro para outro

--- utils/utils.c
+++ utils/utils.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <time.h>
+#include <memory.h>
 #include "utils.h"
 
 struct tm* time_now(void) {
@@ -8,4 +9,11 @@ struct tm* time_now(void) {
   struct tm* _time_now = localtime(&t);
 
   return _time_now;
+}
+
+void* make_ptr_copy(void* source, size_t bytes) {
+  void* dest = malloc(bytes);
+  memcpy(dest, source, bytes);
+
+  return dest;
 }
