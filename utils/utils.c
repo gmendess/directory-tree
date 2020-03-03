25e834633dc25b265b42947d6da61bc21c5133b5
Gabriel Mendes <gabriel.mendespng@gmail.com>
2020-03-02 22:30:17 -0300

    Implementando função tree e __tree, que é uma função auxilar e basicamente o 'cérebro' do comando

--- utils/utils.c
+++ utils/utils.c
@@ -3,6 +3,7 @@
 #include <time.h>
 #include <memory.h>
 #include "utils.h"
+#include "../directory/directory.h"
 
 struct tm time_now(void) {
   time_t t = time(NULL);
@@ -10,3 +11,29 @@ struct tm time_now(void) {
 
   return *_time_now;
 }
+
+static void __tree(Directory* wd) {
+  if(wd == NULL)
+    return;
+
+  // representa a identação do diretório para facilitar visualização
+  static size_t indent = 0;
+
+  printf("%*s%s\n", indent, "", wd->name);
+  Directory* aux = wd->sub_dirs;
+  while(aux) {
+    indent += 2;
+    __tree(aux);
+    aux = aux->next;
+    indent -= 2;
+  }
+}
+
+void tree(void) {
+  // como wd é uma variável static de directory.c, é necessário usar a função pwd para pegar sua referência
+  Directory* wd = pwd();
+  puts("Arvore de diretorios:\n");
+
+  __tree(wd);
+}
+
