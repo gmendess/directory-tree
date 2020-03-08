887bb9b6ba4bb49c67fa7872463b8bc0168d7bfa
Gabriel Mendes <gabriel.mendespng@gmail.com>
2020-03-08 17:16:57 -0300

    time_now agora retorna a cópia do conteúdo apontado pelo retorno de localtime. Descobri que localtime retorna um ponteiro para uma struct tm static, dessa forma, todos os diretórios tinham o membro creation_time iguais, pois apontavam para o mesmo local

--- utils/utils.c
+++ utils/utils.c
@@ -4,9 +4,9 @@
 #include <memory.h>
 #include "utils.h"
 
-struct tm* time_now(void) {
+struct tm time_now(void) {
   time_t t = time(NULL);
   struct tm* _time_now = localtime(&t);
 
-  return _time_now;
+  return *_time_now;
 }
