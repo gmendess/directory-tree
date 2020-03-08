29f0a9629d09f8796789bac99a97714fec27dad1
Gabriel Mendes <gabriel.mendespng@gmail.com>
2020-03-08 00:20:40 -0300

    Criando macro FREE_AND_NULL que libera memória apontada por um ponteiro, que em seguida passa a apontar para NULL

--- utils/utils.h
+++ utils/utils.h
@@ -5,6 +5,12 @@
 #include <stdio.h>
 #include <time.h>
 
+#define FREE_AND_NULL(pointer) \
+do {                           \
+  free(pointer);               \
+  pointer = NULL;              \
+}while(0)
+
 struct tm* time_now(void);
 
 #endif // _UTILS_H_
