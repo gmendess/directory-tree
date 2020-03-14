56111cc27562876b92a284b9d86b76410010cd01
Gabriel Mendes <gabriel.mendespng@gmail.com>
2020-03-14 17:01:10 -0300

    Recolocando o simples parse de comandos em main

--- main.c
+++ main.c
@@ -11,9 +11,35 @@ int main() {
   Directory* wd = pwd();
   char* token;
 
-  mkdir("teste");
-  mkdir("teste/ola");
-  cd("teste/ola");
+  while(1) {
+    char command[50] = {0};
+    printf("%s> ", wd->fullpath);
+    scanf("%[^\n]", command);
+    getchar();
+    token = strtok(command, " ");
+    if(!token)
+      continue;
+    if(strcmp(token, "cd") == 0) {
+      token = strtok(NULL, " ");
+      cd(token);
+    }
+    else if(strcmp(token, "mkdir") == 0) {
+      token = strtok(NULL, " ");
+      if(mkdir(token) == EEXIST)
+        puts("Erro! Caminho passado ja existe!");
+    }
+    else if(strcmp(token, "rmdir") == 0) {
+      token = strtok(NULL, " ");
+      if(rmdir(token) == EPATH)
+        puts("Erro! Caminho passado nao existe!");
+    }
+    else if(strcmp(token, "tree") == 0)
+      tree();
+    else
+      printf("Comando \"%s\" nao encontrado!\n", token);
+
+    wd = pwd();
+  }
 
   return EXIT_SUCCESS;
 }
